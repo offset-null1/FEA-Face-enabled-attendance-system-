c50832fdce940959acc3715629b3289ad5ad6e96
offset-null1 <nirvindak23@gmail.com>
2020-11-05 15:20:40 +0530

    model unstable

---
+++ backend/model/build/CMakeFiles/model.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/daksh/VeriFace/backend/model/src/loss.cpp" "/home/daksh/VeriFace/backend/model/build/CMakeFiles/model.dir/src/loss.cpp.o"
+  "/home/daksh/VeriFace/backend/model/src/resnet.cpp" "/home/daksh/VeriFace/backend/model/build/CMakeFiles/model.dir/src/resnet.cpp.o"
+  )
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "AT_PARALLEL_OPENMP=1"
+  "_THP_CORE"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "/home/daksh/libtorch/include"
+  "/home/daksh/libtorch/include/torch/csrc/api/include"
+  "/usr/local/cuda-11.0/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
