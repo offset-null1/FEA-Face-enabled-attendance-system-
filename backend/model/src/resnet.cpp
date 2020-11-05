fdf5b6d034c2aace367ddb95b20d4f46086cf6a3
offset-null1 <nirvindak23@gmail.com>
2020-11-05 15:24:37 +0530

    model unstable

--- backend/model/src/resnet.cpp
+++ backend/model/src/resnet.cpp
@@ -4,34 +4,49 @@
 #include "../include/loss.h"
 #include "../include/resnet.h"
 
+    torch::Device device_ = torch::kCPU;
 
-    torch::nn::Conv2DOptions setConvOpt(int64_t in_channel, int64_t out_channel, int64_t kernel_size,int64_t stride=1, int64_t padding=0, bool with_bias=false){
-        return torch::nn::Conv2dOptions conv_options (in_channel,out_channel,kernel_size).stride(stride).padding(padding).with_bias(false); 
+    inline torch::nn::Conv2dOptions setConvOpt(int64_t in_channel, int64_t out_channel, int64_t kernel_size=3, int64_t stride=1, int64_t padding=0, bool with_bias=false){
+        return torch::nn::Conv2dOptions(in_channel,out_channel,kernel_size).stride(stride).padding(padding).with_bias(with_bias); 
     }
 
 
-    baseBlock::baseBlock(int64_t inChannel, int64_t outchannel, int64_t stride=1 )
-        :   stride(stride)
-            conv1(),
-            bn1(),
-            conv2(),
-            bn2(),
-            residual(torch::nn::Sequential res_seq_layer()) //copy constructor
+    baseBlock::baseBlockImpl(int64_t inChannel, int64_t outchannel, int64_t stride=1)
+        :   stride(stride),
+            conv1(setConvOpt(inChannel,outchannel,stride)),
+            bn1(outchannel),
+            conv2(setConvOpt(inChannel,outchannel,stride)),
+            bn2(outchannel)
         {
             register_module("conv1", conv1);
             register_module("bn1", bn1);
             register_module("conv2", conv2);
             register_module("bn2", bn2);
 
-            if(!downsample()->is_empty()){
-                register_module("residual", residual);
+            bool condition = (inChannel!=outchannel);
+
+            if(condition){
+                downsample(setConvOpt(inChannel,outchannel,1,2);
+                register_module("downsample", downsample);
+            }
+
+            if(torch::cuda::is_available()){
+                std::cout << "Using CUDA for training" << '\n';
+                device_ = torch::kCUDA;
+                
+                c1->to(device_);
+                c2->to(device_);
+                b1->to(device_);
+                b2->to(device_);
+                if(condition)
+                    downsample->to(device_);
             }
 
         }
 
    
 
-    std::vector<torch::Tensor> baseBlock::forward(torch::Tensor x){
+    torch::Tensor baseBlockImpl::forward(torch::Tensor x){
 
         at::Tensor identity(x.clone());
 
@@ -42,8 +57,8 @@
         x = conv2->forward(x);
         x = bn2->forward(x);
 
-        if(!residual->is_empty()){
-            identity = residual(identity);
+        if(!downsample->is_empty()){
+            identity = downsample->forward(identity);
         }
         x+=identity;
         x = torch::relu(x);
@@ -52,14 +67,14 @@
     }
 
 
-    resnet::resnet(torch::IntList layers, int64_t classes)
+    resnetImpl::resnetImpl(std::vector<int64_t> layers, int64_t classes)
         :   conv1(setConvOpt(3, 64, 7, 2, 3)),
-            bn1(64); //channels
+            bn1(64), //channels
             layer1(make_layer(64, layers[0])),
             layer2(make_layer(128, layers[1], 2)),
             layer3(make_layer(256, layers[2], 2)),
             layer4(make_layer(512, layers[3], 2)),
-            fc(512 * Block.num_seq, classes)
+            fc(512 * num_layers, classes)
         {
             register_module("conv1", conv1);
             register_module("bn1", bn1);
@@ -70,29 +85,8 @@
             register_module("fc", fc);
         }
 
-    
-    void resnet::init_weight(){
-        for(auto m: this->modules()){
-            if(m.value.name() =  "torch::nn::Conv2dImpl"){
-                for (auto p: m.value.parameters()){
-                    torch::nn::init::xavier_normal_(p.value);
-                 }
-            }
-        
-            else if (m.value.name() == "torch::nn::BatchNormImpl"){
-                for (auto p: m.value.parameters()){
-                    if (p.key == "weight"){
-                        torch::nn::init::xavier_normal_(p.value);
-                    }
-                    else if (p.key == "bias"){
-                        torch::nn::init::constant_(p.value, 0);
-                    }
-                }
-            }
-        }
-    }
 
-    torch::Tensor forward(torch::Tensor x){
+    torch::Tensor resnetImpl::forward(torch::Tensor x){
         x = conv1->forward(x);
         x = bn1->forward(x);
         x = torch::relu(x);
