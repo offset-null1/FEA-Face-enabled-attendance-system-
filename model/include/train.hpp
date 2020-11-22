3008d4b4dde2a4c890de5389d1038174b9cea996
offset-null1 <nirvindak23@gmail.com>
2020-11-22 15:50:56 +0530

    stable module

--- model/include/train.hpp
+++ model/include/train.hpp
@@ -2,13 +2,14 @@
 #define __TRAIN__
 
 #include <torch/torch.h>
+#include <torch/script.h>
 #include "load.hpp"
 
 #ifdef __cplusplus
 extern "C"{
 #endif //__cplusplus
 
-    void train_net(loader::loadDataset& data_loader, torch::jit::module net, torch::nn::Linear fc, const int epochs, bool use_default_loss, bool test);
+    void train_net(loader::loadDataset& data_loader, torch::jit::script::Module net, torch::nn::Linear fc, const int epochs, bool use_default_loss, bool test);
 
 #ifdef __cplusplus
 }
