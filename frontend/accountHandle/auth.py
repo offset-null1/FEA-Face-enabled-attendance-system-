bbf34e030aee1c4d01e5725a25b963e929252727
offset-null1 <nirvindak23@gmail.com>
2020-12-12 13:16:34 +0530

    fixed detect

--- frontend/accountHandle/auth.py
+++ frontend/accountHandle/auth.py
@@ -17,7 +17,7 @@ auth_bp = Blueprint(
 
 @auth_bp.route('/login', methods=['GET', 'POST'])
 def login():
-     """
+    """
     Log-in page for registered users.
 
     GET requests serve Log-in page.
@@ -48,7 +48,7 @@ def login():
     
 @auth_bp.route('/signup', methods=['GET', 'POST'])
 def signup():
-      """
+    """
     User sign-up page.
 
     GET requests serve sign-up page.
@@ -61,7 +61,6 @@ def signup():
             user = User(
                 name=form.name.data,
                 email=form.email.data,
-                website=form.website.data
             )
             user.set_password(form.password.data)
             db.session.add(user)
@@ -77,3 +76,17 @@ def signup():
         body="Sign up for a user account."
     )
     
+    
+@login_manager.user_loader
+def load_user(user_id):
+    """Check if user is logged-in on every page load."""
+    if user_id is not None:
+        return User.query.get(user_id)
+    return None
+
+
+@login_manager.unauthorized_handler
+def unauthorized():
+    """Redirect unauthorized users to Login page."""
+    flash('You must be logged in to view that page.')
+    return redirect(url_for('auth_bp.login'))
